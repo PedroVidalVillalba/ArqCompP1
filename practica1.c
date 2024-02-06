1599af9ea4a21de95425d29cda1759a1790cdf3a
Pedro Vidal Villalba <pedrovidalvillalba@gmail.com>
2024-02-06 10:56:20 +0100

    Creado archivo main

---
+++ practica1.c
@@ -0,0 +1,17 @@
+/**
+ * PRÁCTICA 1 ARQUITECTURA DE COMPUTADORES
+ * Jerarquía y comportamiento de la memoria caché: Estudio del efecto de la localidad
+ * de los accesos en las prestaciones de programas en microprocesadores
+ * 
+ * @date 06/02/2024
+ * @authors Cao López, Carlos; Vidal Villalba, Pedro
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+int main(int argc, char** argv) {
+
+}
