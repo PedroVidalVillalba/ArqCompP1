d9ac870109eb76ef0cb859a3d0c29d78a9da99df
CarlosCaoLopez <caolopezcarlos@gmail.com>
2024-02-20 11:49:02 +0100

    Actualizado script y cálculo de R en el programa

--- practica1.c
+++ practica1.c
@@ -11,30 +11,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include "./rutinas_clock.c"
 
-ssize_t S1;     /* Número de líneas caché que caben en la caché L1 de datos */
-ssize_t S2;     /* Número de líneas caché que caben en la caché L2 */
+#define N 10    /* Número de veces que se repite la reducción */
+
+#define min(a, b) ( (a <= b) ? a : b )
+
+/* Tipo de datos de los elementos del array; double por defecto */
+#ifndef USE_INT
+typedef double element_t;
+#else  // USE_INT
+typedef int element_t;
+#endif // USE_INT
+
+/* Tipo de acceso a los elementos del array; a través de un vector de índices por defecto */
+#ifndef DIRECT_ACCESS
+#define index(i) (ind[i])
+#else // DIRECT_ACCESS
+#define index(i) (D * i)
+#endif // DIRECT_ACCESS
 
 
 int main(int argc, char** argv) {
-    double *A;
+    element_t *A;
+    element_t S[N];
     int *ind;
-    int D, L;
-
-    /* Obtener los tamaños de las cachés */
-    S1 = sysconf(_SC_LEVEL1_DCACHE_SIZE) / sysconf(_SC_LEVEL1_DCACHE_LINESIZE); 
-    S2 = sysconf(_SC_LEVEL2_CACHE_SIZE) / sysconf(_SC_LEVEL2_CACHE_LINESIZE);
+    int D, L, R;
+    int i, j;
+    long line_size = sysconf(_SC_LEVEL1_DCACHE_LINESIZE);
 
     /* Procesamos los argumentos */
-
     if (argc != 3){
         fprintf(stderr, "Formato de ejecución: ./acp1 Di Li ");
         exit(EXIT_FAILURE);
     }
+    D = atoi(argv[1]);
+    L = atoi(argv[2]);
 
+    /* Calcular el valor de R necesario para que las lecturas de A[] correspondan a un total de L líneas caché diferentes */
+    R = 1 + (L - 1) * min(1, line_size / ( D * sizeof(element_t) ));
+    
+    /* TODO: justificar esto bien */
+    /* Vamos acceder a R elementos del array avanzando de D en D y queremos L líneas caché.
+     (R-1)*D*sizeof(element_t) te dice cuantos bytes avanzas en total. Si divides line_size, te da cuantas lineas ocupas. L = (R-1)*D*sizeof(element_t)/linesize */
 
-
+    for (j = 0; j < N; j++) {
+        for (i = 0; i < R; i++) {
+            S[j] += A[index(i)];
+        }
+    }
     
     
 
--- script_experimentos.sh
+++ script_experimentos.sh
@@ -3,17 +3,30 @@
 #SBATCH -n 1 -c 64 -t 02:00:00 --mem=256G
 # Ponemos nombre a nuestro trabajo para poder identificarlo.
 # ATENCIÓN - Debes sustituir el NN por el número de equipo.
-#SBATCH --job-name p1acgNN
+#SBATCH --job-name p1acg09
 
 # Sustituir los valores de Di y Li por los calculados para la realización de la práctica.
 
-gcc acp1.c -o acp1 -msse3 -O0
 
+# Opciones de compilación obligatorias
+FLAGS="-msse3 -O0"
+
+# Variantes de compilación para cambiar el comportamiento del programa.
+# Comentar y descomentar para cambiar el funcionamiento.
+FLAGS+=" -D USE_INT"				# Usar datos de tipo int en lugar de double
+FLAGS+=" -D DIRECT_ACCESS"		# Utilizar referencias directas a los elementos de A[] en lugar de a través del vector de índices
+
+gcc acp1.c -o acp1 $FLAGS
+
+S1=$[$(getconf LEVEL1_DCACHE_SIZE)/$(getconf LEVEL1_DCACHE_LINESIZE)]
+S2=$[$(getconf LEVEL2_CACHE_SIZE)/$(getconf LEVEL2_CACHE_LINESIZE)]
+
+#Repetimos el experimento 10 veces
 for i in {1..10}
 do
-	for D in {D1,D2,D3,D4,D5}
+	for D in {$[1<<0],$[1<<],$[1<<],$[1<<],$[1<<]}
 	do
-		for L in {L1,L2,L3,L4,L5,L6,L7}
+		for L in {$[$S1/2],$[3*$S1/2],$[$S2/2],$[3*$S2/4],$[2*$S2],$[4*$S2],$[8*S2]}
 		do
 			./acp1 $D $L
 		done
