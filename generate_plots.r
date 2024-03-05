ef2a018181c959c83ca3b00a281e9cda9e7d8177
Pedro Vidal Villalba <pedrovidalvillalba@gmail.com>
2024-03-05 18:42:32 +0100

    Datos obtenidos del Cesga

--- generate_plots.r
+++ generate_plots.r
@@ -1,17 +1,28 @@
-datos = read.csv(file("stdin"), header = TRUE)
-datos = aggregate(cbind(Clocks, ClocksPerAccess) ~ D + L + R, datos, mean)
-datos = datos[order(datos$D, datos$L, datos$R), ]
-datos
+data = read.csv(file("stdin"), header = TRUE)
+data = na.omit(data)  # Eliminar NAs (últimas filas)
+means = aggregate(cbind(Clocks, ClocksPerAccess) ~ D + L + R, data, mean)
+sds = aggregate(cbind(Clocks, ClocksPerAccess) ~ D + L + R, data, sd)
+means = means[order(means$D, means$L, means$R), ]
+sds = sds[order(sds$D, sds$L, sds$R), ]
 
-plot(datos$L, datos$ClocksPerAccess, pch = 19, col = factor(datos$D),
+plot(means$L, means$ClocksPerAccess, pch = 19, col = factor(means$D),
      main = "Medidas de rendimiento en accesos a memoria",
      xlab = "Número de líneas caché leídas (L)",
      ylab = "Ciclos de reloj por acceso",
      xaxt = 'n', log = "x")
-axis(side = 1, at = unique(datos$L), las = 2)
+axis(side = 1, at = unique(means$L), las = 2)
 
 i = 1
-for (D in unique(datos$D)) {
-    points(datos[datos$D == D, ]$L, datos[datos$D == D, ]$ClocksPerAccess, type = 'l', col = i)
+for (D in unique(means$D)) {
+    x = means[means$D == D, ]$L
+    y = means[means$D == D, ]$ClocksPerAccess
+    y.sd = sds[sds$D == D, ]$ClocksPerAccess
+    points(x, y, type = 'l', col = i)
+    arrows(x0 = x, x1 = x, y0 = y - y.sd, y1 = y + y.sd, 
+           code = 3, angle = 90, length = 0.1, col = i)
+    # legends = c(legends, sprintf("D = %d", D))
     i = i + 1
 }
+
+legends = sprintf("D = %d", unique(means$D))
+legend("topleft", legends, fill = 1:5)
--- process_output.sh
+++ process_output.sh
@@ -5,11 +5,14 @@ echo "D,L,R,Clocks,ClocksPerAccess"
 declare -A clocks_by_param
 declare -A clocks_per_access_by_param
 
+IFS=''
+read # Leer primera línea, que es un resumen del Finisterrae
 while IFS='=,' read _ D _ L _ R; do
+    if [ "$D" = "" ]; then exit; fi     # Salir si no se leen datos (líneas finales del Finisterrae)
     IFS='=.'
-    read -r _ clocks _
+    read _ clocks _
     IFS='='
-    read -r _ clocks_per_access
+    read _ clocks_per_access
     read
 
     echo "$D,$L,$R,$clocks,$clocks_per_access"
